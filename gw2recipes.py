246e952087abb69f62fd4edab922bf233d28046b
elfez <dswegen@gmail.com>
2013-01-19 14:14:14 +0000

    - Cleaned up variable names - Added caching of data - Added columns for crafting and flip profit

--- gw2recipes.py
+++ gw2recipes.py
@@ -2,58 +2,59 @@
 
 import gw2spidy
 from html import HTML
+import cPickle
+from os import path
+import time
 import cgitb
 cgitb.enable()
 
-'''
-recipe:
-    'result_item_min_sale_unit_price
-    rating
-    crafting_cost
-    name
-    data_id
-    result_item_max_offer_unit_price
-    result_item_data_id
-    discipline_id
-    result_count
-'''
+CACHEDIR = "/tmp"
+MODTIME = 30 * 60       # Time in seconds to cache data
 
 disc = ['Huntsman', 'Artificer', 'Weaponsmith', 'Armorsmith', 'Leatherworker',
         'Tailor', 'Jeweler', 'Cook']
 
 class recipe:
 
-    def __init__ (self, name, buyPrice, sellPrice, cost, itemId, disc):
+    def __init__ (self, name, buyPrice, sellPrice, matCost, itemId, disc, 
+            skill):
         self.name = name
         self.buyPrice = buyPrice
-        self.sellPrice = sellPrice,
-        self.cost = cost
+        self.sellPrice = sellPrice
+        self.matCost = matCost
         self.itemId = itemId
         self.disc = disc
-        self.profit = buyPrice - cost
+        self.flipProfit = (self.sellPrice * 0.85) - self.buyPrice
+        self.instantProfit = (self.buyPrice * 0.85) - self.matCost
+        self.craftProfit = (self.sellPrice * 0.85) - self.matCost
+        self.skill = skill
 
     def isProfitable(self):
-        if self.profit > 50:
+        if (self.flipProfit or self.instantProfit or self.craftProfit)> 50:
             return True
-
-    def printData(self):
-        print "%-30s: %5d %5d %.2f" % (self.name, self.cost, self.buyPrice,
-                (self.profit * 0.85) / 100.0)
 
     def genTR(self):
         tr = HTML('tr')
 
         url = "http://www.gw2spidy.com/recipe/%d" % self.itemId
+        # Name
         tr.td.a(self.name, href=url, target="_blank")
-        marginPct = self.profit / float(self.buyPrice)
-        for i in (self.cost/100.0, self.buyPrice/100.0, 
-                self.profit/ 100.0, "%.2f" % (marginPct * 100)):
-            tr.td(str(i), align="right")
+        for i in (self.toS(self.matCost),
+                self.toS(self.buyPrice), 
+                self.toS(self.sellPrice),
+                self.toS(self.instantProfit),
+                self.toS(self.flipProfit),
+                self.toS(self.craftProfit)):
+                
+
+            tr.td(i, align="right")
+            # Profession
         tr.td(disc[self.disc])
+        tr.td(str(self.skill))
         return (tr)
 
     def toS(self, val):
-        return val / 100.0
+        return "%.2f" % (val / 100.0)
 
 def populateScripts(ent):
 
@@ -81,7 +82,7 @@ function filter2 (phrase, _id){
 $(document).ready(function()
 {
 
-  $("#recipes").tablesorter({widgets:['zebra'], sortList: [[3,1],],});
+  $("#recipes").tablesorter({widgets:['zebra'], sortList: [[4,1],],});
 
 });
 """
@@ -94,7 +95,36 @@ $(document).ready(function()
     body.script(" ", escape=False, type="text/javascript", 
             src="http://www.magweb.net/static/js/tablesorter/jquery.tablesorter.js")
     body.script(scr2, escape=False, type="text/javascript")
-    
+
+def getRecipes(d):
+
+    recipes= []
+    cSkill = disc[d]
+    datafile = "%s/%s_data.pkl" % (CACHEDIR, cSkill)
+
+    fetchData = False
+    if path.exists(datafile):
+        if time.time() - path.getmtime(datafile) > MODTIME:
+            fetchData = True
+    else:
+        fetchData = True
+
+    if fetchData:
+        res = gw2.getRecipesOfDiscipline(d+1, allPages = True)
+        for i in res:
+            newItem = recipe(i['name'], i['result_item_max_offer_unit_price'],
+                    i['result_item_min_sale_unit_price'], i['crafting_cost'],
+                    i['data_id'], d, i['rating'])
+            recipes.append(newItem)
+
+        output = open(datafile, 'wb')
+        cPickle.dump(recipes, output)
+    else:
+        infile = open(datafile, 'rb')
+        recipes = cPickle.load(infile)
+        infile.close()
+
+    return recipes
 
 
 gw2 = gw2spidy.Gw2Spidy()
@@ -104,27 +134,28 @@ print "Content-type: text/html\n"
 page = HTML('html')
 body = page.body()
 populateScripts(body)
+body.ul.li("All prices in silver")
+body.ul.li("Instant profit = craft, sell to highest buyer")
+body.ul.li("Flip profit = buy at highest buyer, sell at lowest seller")
+body.ul.li("Craft profit = Craft, sell at lowest seller")
 
 form = body.form("Filter:", style="margin-left: 10px;")
 form.input(' ', type="text", name="filtrpt", onkeyup="filter2(this,'recipes')")
 tab = body.table(id="recipes", klass="tablesorter", border="1")
 thead = tab.thead()
 thead.th("Name")
-thead.th("Cost")
-thead.th("Buy")
-thead.th("Profit")
-thead.th("Margin %")
+thead.th("Mat Cost")
+thead.th("Highest Buy")
+thead.th("Lowest Sell")
+thead.th("Instant Profit")
+thead.th("Flip Profit")
+thead.th("Craft Profit")
 thead.th("Profession")
+thead.th("Skill")
 
 for d in range(len(disc)):
-    #print "====== %s" % disc[d]
     recipes = []
-    res = gw2.getRecipesOfDiscipline(d+1, allPages = True)
-    for i in res:
-        newItem = recipe(i['name'], i['result_item_max_offer_unit_price'],
-                i['result_item_min_sale_unit_price'], i['crafting_cost'],
-                i['data_id'], d)
-        recipes.append(newItem)
+    recipes.extend(getRecipes(d))
 
     for i in recipes:
         if i.isProfitable():
